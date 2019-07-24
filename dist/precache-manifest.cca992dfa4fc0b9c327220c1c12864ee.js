9f963ffc4ffdf9b6303db0abb3fd6c402a9c1a42
shiyu111 <46973872+shiyu111@users.noreply.github.com>
2019-07-23 18:17:25 -0700

    Add files via upload

---
+++ dist/precache-manifest.cca992dfa4fc0b9c327220c1c12864ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ed2e8d4839ded216bd37",
+    "url": "/js/chunk-vendors.cc3142d8.js"
+  },
+  {
+    "revision": "941f8b3c606d07889a8c",
+    "url": "/js/app.c2fea4c8.js"
+  },
+  {
+    "revision": "915d4d87f96565a473eea023263c67f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09292baf43eb8a73750d627b44708af",
+    "url": "/img/logo_UCSD.e09292ba.png"
+  },
+  {
+    "revision": "5853be6e0dffcae67123c0a5df48b623",
+    "url": "/img/crest.5853be6e.png"
+  },
+  {
+    "revision": "941f8b3c606d07889a8c",
+    "url": "/css/app.5c147926.css"
+  }
+];
